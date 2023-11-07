0997f7576798a5f1a7c5287da023bcce0efb2c8a
Java13320453 java13320453@ldap.com <Java13320453 java13320453@ldap.com>
2023-11-07 18:50:36 +0530

    After_Nested_Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
     template: `<div>
-                    <h1>{{pageHeader}}</h1>                  
+                    <h1>{{pageHeader}}</h1>
+                    <app-student></app-student>                  
                </div>`
 })
 export class AppComponent {
