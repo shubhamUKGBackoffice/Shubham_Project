0d638abb3ad1f519e3ae107b28ef18e780266e7b
Java13320453 java13320453@ldap.com <Java13320453 java13320453@ldap.com>
2023-11-07 16:06:19 +0530

    Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  template: "<h3>  welcome To Angular Learning <h3>",
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
