870fa341a4da7dbca0f1ed161e0aac351abcb699
Java13320453 java13320453@ldap.com <Java13320453 java13320453@ldap.com>
2023-11-09 16:19:30 +0530

    HTML_Attribute_DOM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
     template: `<div>
-                    <img [src] = 'ImagePath' />                
+                  <input id='inputId' type='text' value='Anurag'>    
                </div>`
 })
 export class AppComponent {
-  ImagePath : string = "https://dotnettutorials.net/wp-content/uploads/2019/09/cropped-dotnettutorials.png";
 }
