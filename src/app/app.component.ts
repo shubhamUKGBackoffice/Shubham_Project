b29ce99d656455c659b0c2d7ee94b93825fb5bd9
Java13320453 java13320453@ldap.com <Java13320453 java13320453@ldap.com>
2023-11-10 13:25:19 +0530

    Attribute_binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
-    template: `<div>
-                  <input id='inputId' type='text' value='Anurag'>    
-               </div>`
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    pageHeader: string = 'Student Details';
+    FirstName: string = 'Anurag';
+    LastName: string = 'Mohanty';
+    Branch: string = 'CSE';
+    Mobile: number = 9876543210;
+    Gender: string = 'Male';
+    Age: number = 22;
 }
